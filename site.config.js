823da4eb71c4172013b5846d0c034262e0fc38ad
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:50:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: '정보이용료현금화 신뢰할 수 있는 파트너. 간편 인증만으로 대기 시간 없이 당일 초고속 처리를 보장합니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: '233f495b0eb1d11c142d41d08d99e53df524de57',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
