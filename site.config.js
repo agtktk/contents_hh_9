97a1b064dc161e3074133ab137e56c6f7a3a1e32
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:54:00 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: '정보이용료현금화 희망티켓',
+  siteUrl: 'https://contents-hh-9.pages.dev',
+  siteDescription: '정보이용료현금화 신뢰할 수 있는 파트너. 간편 인증만으로 대기 시간 없이 당일 초고속 처리를 보장합니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
